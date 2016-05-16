ab02e9a36561291b82d153a2e4374109c996107d
Niklas Tinglöf <niklo@surdeg.se>
2016-05-16 11:53:03 +0200

    further stub coding...

--- src/sous_vide/sous_vide.ino
+++ src/sous_vide/sous_vide.ino
@@ -34,7 +34,9 @@ enum
 {
   ERROR_NONE = 0,
   
-  ERROR_WATER_LEVEL,    // 
+  ERROR_INIT,           // init failed...
+  
+  ERROR_WATER_LEVEL,    // water level too low...
   
   // 
   ERROR_UNKNOWN = -1
@@ -86,7 +88,17 @@ void heater_setup()
   todo: implement...
 }
 
+void heater_control(uint8_t on_off)
+{
+  todo: implement...
+}
+
 void pump_setup()
+{
+  todo: implement...
+}
+
+void pump_control(uint8_t on_off)
 {
   todo: implement...
 }
@@ -95,6 +107,22 @@ void temp_sensor_setup()
 {
   todo: implement...
 }
+
+uint8_t temp_sensor_read()
+{
+  todo: implement...
+}
+
+void level_sensor_setup()
+{
+  todo: implement...
+}
+
+bool level_sensor_read()
+{
+  todo: implement...
+}
+
 
 //************
 // ui code...
@@ -119,6 +147,19 @@ void controller_setup()
 // function to be called each turn of the arduino loop...
 void controller_loop()
 {
+  // todo: read (and check) water level...
+  if (!level_sensor_read())
+  {
+    // error...
+  }
+  
+  // todo: read temperature...
+  
+  // todo: control algo...
+  
+  // todo: operate pump...
+  // todo: operate heater...
+  
 }
 
 //**************
@@ -136,5 +177,30 @@ void wifi_loop()
 //*****************
 // utility code...
 //*****************
+void set_state(uint8_t state)
+{
+  // todo: sanity check!?
+  g_main_state = state;
+}
 
+uint8_t get_state()
+{
+  return g_main_state;
+}
+
+uint8_t check_state(uint8_t state)
+{
+  return (g_main_state == uint8_t state);
+}
+
+void set_error(uint8_t error)
+{
+  g_error_code = error;
+  g_main_state = MAIN_STATE_ERROR; // ???
+}
+
+uint8_t get_error()
+{
+  return g_error_code;
+}
 
